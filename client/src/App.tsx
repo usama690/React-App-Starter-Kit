d38a5e6fb45baff1557f89a1b8b3c18334a43959
muzair-khan <uzairkhan1486@gmail.com>
2023-09-28 05:04:53 +0500

    set the app config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import Routes from './routes/Routes'
-import UserContainer from './context/UserContext'
-// import { SnackbarMessage } from './components/core/SnackbarMessage'
+import Routes from './routes'
+import { Provider } from 'react-redux'
+import store from './store'
+import './styles/global.css'
 
 export default function App(): JSX.Element {
     return (
-        <HelmetProvider>
+        <Provider store={store}>
             <BrowserRouter>
-                <UserContainer>
-                    <Routes />
-                </UserContainer>
+                <Routes />
             </BrowserRouter>
-            {/* <SnackbarMessage /> */}
-        </HelmetProvider>
+        </Provider>
     )
 }
